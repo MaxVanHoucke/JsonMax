468875dec96731b699e126ce2cb81701721e16b6
Max Van Houcke <max.vanhoucke@student.uantwerpen.be>
2019-04-30 22:29:06 +0200

    Added test and examples directory

---
+++ examples/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "../single_include/JsonMax.h"
+
+using namespace JsonMax;
+
+int main(int argc, char** argv) {
+
+    std::string fileName = "../../examples/weather.json";
+    Object weather = parseFile(fileName).getObject();
+
+    std::cout << "Type: " << weather["type"].getString() << std::endl;
+
+    for (const Element& forecast: weather["days"].getArray()) {
+
+        if (!forecast.isObject()) {
+            continue;
+        }
+
+        for (const Pair& pair: forecast.getObject().pairs()) {
+            std::cout << pair.getKey() << ": " << pair.getValue().toString() << std::endl;
+        }
+    }
+
+    return 0;
+}
---
+++ test/ParsingTest.cpp
@@ -0,0 +1,39 @@
+/**
+ * @author Max Van Houcke
+ */
+
+#include "catch.h"
+#include "../src/Parser.h"
+
+using namespace JsonMax;
+
+TEST_CASE( "Happy days parsing", "[parsing]" ) {
+
+    Object obj = parseFile("../../test/input/example.json").getObject();
+
+    CHECK(obj["string"].getString() == "string");
+    CHECK(obj["number"].getInt() == 1);
+    CHECK((obj["float"].getDouble() < 3.15 && obj["float"].getDouble() > 3.13));
+    CHECK(obj["null"].isNull());
+    CHECK(obj["bool"].getBool());
+    CHECK(obj["object"]["object"]["object"]["string"].getString() == "string");
+    CHECK(obj["empty object"].getObject().empty());
+
+    for (const Element& element: obj["array"].getArray()) {
+        if (element.isObject()) {
+            CHECK(element.getObject()["type"].getString() == "object");
+        }
+        else if (element.isInt()) {
+            CHECK(element.getInt() == 1);
+        }
+        else if (element.isDouble()) {
+            CHECK((element.getDouble() < 3.15 && element.getDouble() > 3.13));
+        }
+        else if (element.isString()) {
+            CHECK(element.getString() == "string");
+        }
+        else if (element.isArray()) {
+            CHECK(element.getArray()[0].getString() == "another array");
+        }
+    }
+}
